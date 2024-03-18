f0f18c2079f03ed8b8ef755077b9dc0fdc711ae3
oscariolo <57816024+oscariolo@users.noreply.github.com>
2024-03-18 17:51:10 -0500

    intento de dash

--- Player/wizard.gd
+++ Player/wizard.gd
@@ -7,6 +7,9 @@ const FALL_CROUCH_BOOST = 50
 const base_jump_force =  -20000
 var base_speed = 10000 #* 60
 var facing_direction = 0
+var canDash = true
+var is_dashing = false
+
 
 func _ready():
 	$Animations.play("walking")
@@ -22,7 +25,8 @@ func _manage_movement(delta):
 	#move left and right
 	velocity.x = base_speed * facing_direction * delta
 	#gravity
-	velocity.y += GRAVITY * delta
+	if velocity.y < FALL_LIMIT_VELOCITY:
+		velocity.y += GRAVITY * delta
 
 	#jump mechanic
 	#base jump
@@ -40,13 +44,36 @@ func _manage_movement(delta):
 	#fall compensation
 	if velocity.y <= 0 and Input.is_action_just_released("jump"): #jumping state and release jump
 		velocity.y += FALL_COMPENSATION
-
-
+	
+	#dash jump
+	if Input.is_action_just_pressed("dash"):
+		_dash_jump(delta)
+	
+	velocity.x = lerp(velocity.x,0.0,0.1)
+	
 	#base gravity
-	if velocity.y > FALL_LIMIT_VELOCITY and !Input.is_action_pressed("crouch"):
-		velocity.y = FALL_LIMIT_VELOCITY
 
 
+func _dash_jump(delta):
+	if canDash:
+		var dash_direction = Input.get_vector("move_left","move_right","face_up","face_down")
+		#velocity = velocity + dash_direction.normalized() * Vector2(1000,1000)
+		#print(velocity)
+		dash_direction = dash_direction.normalized()
+		if abs(dash_direction.x) > 0.5:
+			dash_direction.x = sign(dash_direction.x)
+		elif abs(dash_direction.x) < 0.5:
+			dash_direction.x = sign(dash_direction.x)
+		
+		if abs(dash_direction.y) > 0.5:
+			dash_direction.y = sign(dash_direction.y)
+		elif abs(dash_direction.y) < 0.5:
+			dash_direction.y = sign(dash_direction.y)
+			
+		velocity = dash_direction * Vector2(2000*60,400*60) * delta
+		
+	
+	
 func _buffer_jump():
 	$buffer_jump.start()
 
